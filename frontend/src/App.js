d6c0bddd3c3cef7735c23cb9142f605ed7273db5
bbpsk <bbpsk17@gmail.com>
2021-10-22 18:22:34 -0400

    fixed hotel cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,30 +4,27 @@ import axios from 'axios'
 import Nav from './components/Nav'
 import HotelList from './components/HotelList';
 
-function App() {
-  const [logged, setLogged] = useState('false');
+const App = () => {
+  const [logged, setLogged] = useState(false);
   const [featured, setFeatured] = useState([]);
 
   useEffect(() => {
     (async () => {
-        const {data} = await axios.get('http://localhost:8080/api/hotel'); // axios gives back {data: {}}
+        const {data} = await axios.get('http://localhost:8080/api/hotel');
         console.log(data);
         setFeatured(data);
     })();
   }, []);
-
-  const hotel = featured[0];
-  console.log(hotel);
+  
   return (
-    
     <div>
       <Nav logged={logged}/>
       <Router>
           <Switch>
               <Route path="/" exact>
                   <>
-                  <h1 className="p-2">Spinka</h1>
-                  <HotelList hotels={featured}></HotelList>
+                  <h1 className="p-2">Spinka Hotels</h1>
+                  <HotelList hotels={featured}/>
                   
                   </>
               </Route>
--- frontend/src/components/HotelList.jsx
+++ frontend/src/components/HotelList.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Card from './Card'
 
 const HotelList = (props) => {
-    console.log('inside list', props.hotels[0]);
+    console.log('inside list', props.hotels);
     return (
+        props.hotels.length == 0 ? <h3>Not loaded yet</h3>
+        :
         <>
         <Card hotel={props.hotels[0]}></Card>
         <Card hotel={props.hotels[1]}></Card>
         <Card hotel={props.hotels[2]}></Card>
         </>
+        
+        
     )
 }
 
