b021526469f8fd5623eba866e1be6b174f7b76f6
Jehyun <congmul@hotmail.com>
2021-10-23 01:15:12 -0700

    Tracking user

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,17 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './colors.css'
 import SignForm from './SignForm/SignForm';
+import Auth from '../utils/auth';
 
 const Nav = (props) => {
     let message = ''
     if(props.logged) message = `Welcome ${props.name}`;
     else message = `Welcome guest`;
 
+    const [ user, setUser ] = useState({});
     const [ isSignForm, setIsSignForm ] = useState(false);
 
     const signFormOnClick = () =>{
         setIsSignForm(true)
     }
+
+    useEffect(async () => {
+       if(Auth.loggedIn()){
+            const user = await Auth.getProfile();
+            console.log(user);
+            setUser(user.data);
+       }
+    }, [])
 
     return (
         <nav className="navbar navbar-expand navbar-dark blue">
@@ -25,11 +35,20 @@ const Nav = (props) => {
                 <li className="nav-item">
                     <Link to="/" className="nav-link">Hotels</Link>
                 </li>
-                <li className="nav-item">{`${message}`}</li>
-                <li className="nav-item">
-                    <Link to="#" onClick={signFormOnClick} className="nav-link">Sign In or Join</Link>
-                    {isSignForm ? <SignForm setIsSignForm={setIsSignForm} /> : <></>}
-                </li>
+                {/* <li className="nav-item">{`${message}`}</li> */}
+                {Auth.loggedIn() ? (<>
+                    <li className="nav-item">
+                        <Link to="#" className="nav-link">Hello, {user.username? user.username : "World"}</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="#" onClick={Auth.logout} className="nav-link">Logout</Link>
+                    </li>
+                </>) : (
+                    <li className="nav-item">
+                        <Link to="#" onClick={signFormOnClick} className="nav-link">Sign In or Join</Link>
+                        {isSignForm ? <SignForm setIsSignForm={setIsSignForm} /> : <></>}
+                    </li>
+                ) }
                 
             </ul>
             </div>
