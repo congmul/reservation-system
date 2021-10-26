dd9a60ca7b7df93e847829cbc62ad710af34e73a
Jehyun <congmul@hotmail.com>
2021-10-26 13:16:48 -0700

    Update Docker environment

--- frontend/src/utils/user-API.jsx
+++ frontend/src/utils/user-API.jsx
@@ -1,20 +1,27 @@
 import axios from 'axios'
 
+
+const baseURL = process.env.REACT_APP_BASE_URL || `http://localhost:8080/api/user/`
+
+const api = axios.create({
+    baseURL
+})
+
 export const loginUser = async (userData) => {
     console.log(userData);
-    return await axios.post('http://localhost:8080/api/user/login', userData);
+    return await api.post('/user/login', userData);
 };
 
 // SignUp
 export const createUser = async (userData) => {
     console.log(userData);
-    return await axios.post('http://localhost:8080/api/user/', userData);
+    return await api.post('/user/', userData);
 }
 
 // Get one user
 export const getSingleUser = async (userData, token) => {
     // console.log(token);
-    return await axios.get(`http://localhost:8080/api/user/me/`,{ 
+    return await api.get(`/user/me/`,{ 
         headers: {
             'Content-Type': 'application/json',
             authorization: `Bearer ${token}`
@@ -22,3 +29,26 @@ export const getSingleUser = async (userData, token) => {
         }
     );
 }
+
+// export const loginUser = async (userData) => {
+//     console.log(userData);
+//     return await axios.post('http://localhost:8080/api/user/login', userData);
+// };
+
+// // SignUp
+// export const createUser = async (userData) => {
+//     console.log(userData);
+//     return await axios.post('http://localhost:8080/api/user/', userData);
+// }
+
+// // Get one user
+// export const getSingleUser = async (userData, token) => {
+//     // console.log(token);
+//     return await axios.get(`http://localhost:8080/api/user/me/`,{ 
+//         headers: {
+//             'Content-Type': 'application/json',
+//             authorization: `Bearer ${token}`
+//             }
+//         }
+//     );
+// }
