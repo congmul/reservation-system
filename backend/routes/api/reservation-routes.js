a7d37f02c3a9f7d029a92468266b56da81ce61e2
Jehyun <congmul@hotmail.com>
2021-10-27 15:16:21 -0700

    Add Create reservation routes

--- backend/routes/api/reservation-routes.js
+++ backend/routes/api/reservation-routes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const { getReservation } = require('../../controller/reservation-controller');
+const { getReservation, createReservation } = require('../../controller/reservation-controller');
 
 // PATH:  /api/reservation/
 router.get('/', async (req, res) => {
     try {
         const response = await getReservation();
         console.log(response);
+        res.json(response);
+    }catch(error){
+        throw error;
+    }
+})
+
+router.post('/:username', async (req, res) => {
+    try {
+        console.log(req.body);
+        const response = await createReservation(req.body, req.params.username);
+        // console.log(response);
         res.json(response);
     }catch(error){
         throw error;
