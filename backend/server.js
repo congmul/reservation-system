3f8001506559dc10c4568c73dd311731ebb99123
Jehyun <congmul@hotmail.com>
2021-11-11 15:19:18 -0800

    Add path Package

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 const { Hotel, Reservation, User } = require("./models");
 require('dotenv').config();
 const dbConnection = require('./config/connection.js');
