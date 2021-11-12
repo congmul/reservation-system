ac564286945f211e7df5648b6382289d14dd5c94
Jehyun <congmul@hotmail.com>
2021-11-12 07:47:11 -0800

    Test for API url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const dbConnection = require('./config/connection.js');
 
 
 const app = express();
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extened:  true}))
